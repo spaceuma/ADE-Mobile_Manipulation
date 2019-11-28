19d7c1fd5f7dec31fc96e8cb7f99046e51e031d6
Ricardo Sanchez <ricardosan@uma.es>
2019-11-28 13:18:29 +0000

    Trajectory control test changed from MotionPlanTest to MMExecutorTest
    Signed-off-by: Ricardo Sanchez <ricardosan@uma.es>

--- test/unit/MobileManipExecutorTests.cpp
+++ test/unit/MobileManipExecutorTests.cpp
@@ -1,1 +1,30 @@
 #include <gtest/gtest.h>
+#include "Waypoint.hpp"
+#include "WaypointNavigation.hpp"
+#include "MobileManipExecutor.h"
+
+using namespace waypoint_navigation_lib;
+
+TEST(MMExecutorTest, trajectorycontrol){
+  MobileManipExecutor dummyExecutor;
+  WaypointNavigation pathTracker;
+
+  Waypoint initWaypoint;
+  initWaypoint.position[0] = 0.0;
+  initWaypoint.position[0] = 0.0;
+
+  Waypoint endWaypoint;
+  endWaypoint.position[0] = 0.0;
+  endWaypoint.position[0] = 0.0;
+
+  std::vector<Waypoint> dummyPath;
+
+  dummyPath.push_back(initWaypoint);
+  dummyPath.push_back(endWaypoint);
+
+
+  Pose robotPose;
+  pathTracker.setPose(robotPose);
+  
+
+}
