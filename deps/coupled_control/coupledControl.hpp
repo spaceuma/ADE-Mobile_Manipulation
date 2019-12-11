4c7ba3d162dd26ca81d3e2f31307a89c4dce83eb
Gonzalo Paz Delgado <gonzalopd96@uma.es>
2019-12-11 13:22:56 +0100

    Change assignment procedure to select next manipulator configuration

--- deps/coupled_control/coupledControl.hpp
+++ deps/coupled_control/coupledControl.hpp
@@ -1,14 +1,14 @@
 #ifndef _COUPLED_CONTROL_HPP_
 #define _COUPLED_CONTROL_HPP_
 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <iostream>
-#include <vector>
-#include <math.h>
-#include <cstdlib>
-#include "Waypoint.hpp"
 #include "MotionCommand.h"
+#include "Waypoint.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
 
 #define PI 3.1415
 
@@ -18,18 +18,33 @@ using namespace std;
 
 namespace coupled_control
 {
-    class coupledControl
-    {
-        public: 
-			int  findMaxValue(std::vector<float> vect);
-            void modifyMotionCommand(double mMaxSpeed, double maxJW, std::vector<float>& jW, MotionCommand rover_command, MotionCommand& modified_rover_command);
-			void selectNextManipulatorPosition(int current_waypoint, std::vector<int> assign, std::vector<double> armConfig, std::vector<double>& nextConfig, int negative);
-			void manipulatorMotionControl(double gain, int& saturation, double mMaxSpeed, std::vector<double> nextConfig, std::vector<double> lastConfig, std::vector<float>& jW);
-			double constrainAngle(double angle, int negative);
+class coupledControl
+{
+public:
+    int findMaxValue(std::vector<float> vect);
 
-			
-    };
+    void modifyMotionCommand(double mMaxSpeed,
+                             double maxJW,
+                             std::vector<float> &jW,
+                             MotionCommand rover_command,
+                             MotionCommand &modified_rover_command);
+
+    void selectNextManipulatorPosition(int current_waypoint,
+                                       std::vector<int> *assign,
+                                       std::vector<std::vector<double>> *armConfig,
+                                       std::vector<double> *nextConfig,
+                                       int negative);
+
+    void manipulatorMotionControl(double gain,
+                                  int &saturation,
+                                  double mMaxSpeed,
+                                  std::vector<double> nextConfig,
+                                  std::vector<double> lastConfig,
+                                  std::vector<float> &jW);
+
+    double constrainAngle(double angle, int negative);
+};
 
 } // end namespace coupled_control
 
-#endif 
+#endif
