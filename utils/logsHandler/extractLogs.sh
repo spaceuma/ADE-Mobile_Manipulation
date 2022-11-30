27479d76139c97c977f3f4658ca2c327f4b79d96
Gonzalo J. Paz Delgado <gonzalopd96@uma.es>
2022-11-30 12:24:40 +0100

    Keep extracting logs even if something fails

--- utils/logsHandler/extractLogs.sh
+++ utils/logsHandler/extractLogs.sh
@@ -7,16 +7,19 @@ fi
 logs=$PWD
 rm -rf $logs/extractedLog
 mkdir extractedLog
-cd $logs/$1/execution/MobileManipulation/data
-cp *armMotionProfile.txt $logs/extractedLog/armMotionProfile.txt
-cp *costMap.txt $logs/extractedLog/costMap.txt
-cp *elevationMap.txt $logs/extractedLog/elevationMap.txt
-cp *offset.txt $logs/extractedLog/offset.txt
-cp *roverPath.txt $logs/extractedLog/roverPath.txt
-cp *sdMap.txt $logs/extractedLog/sdMap.txt
-cp *slopeMap.txt $logs/extractedLog/slopeMap.txt
-cp *traversabilityMap.txt $logs/extractedLog/traversabilityMap.txt
-cp *validityMap.txt $logs/extractedLog/validityMap.txt
-cp *wristPath.txt $logs/extractedLog/wristPath.txt
-cp *3dcostMap $logs/extractedLog/3dcostMap.txt
+cp $logs/$1armProfile.txt $logs/extractedLog/armMotionProfile.txt || true
+cp $logs/$1costMap.txt $logs/extractedLog/costMap.txt || true
+cp $logs/$1elevationMap.txt $logs/extractedLog/elevationMap.txt || true
+cp $logs/$1offset.txt $logs/extractedLog/offset.txt || true
+cp $logs/$1roverPath.txt $logs/extractedLog/roverPath.txt || true
+cp $logs/$1sdMap.txt $logs/extractedLog/sdMap.txt || true
+cp $logs/$1slopeMap.txt $logs/extractedLog/slopeMap.txt || true
+cp $logs/$1traversabilityMap.txt $logs/extractedLog/traversabilityMap.txt || true
+cp $logs/$1validityMap.txt $logs/extractedLog/validityMap.txt || true
+cp $logs/$1wristPath.txt $logs/extractedLog/wristPath.txt || true
+cp $logs/$13dcostMap.txt $logs/extractedLog/3dcostMap.txt || true
+cp $logs/$1roverPos.txt $logs/extractedLog/roverPos.txt || true
+cp $logs/$1armPos.txt $logs/extractedLog/armPos.txt || true
+cp $logs/$1armCommand.txt $logs/extractedLog/armCommand.txt || true
+cp $logs/$1motionCommand.txt $logs/extractedLog/motionCommand.txt || true
 cd $logs
