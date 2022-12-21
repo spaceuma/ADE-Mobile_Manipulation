6e5c9aea9cbb8917576c31ad33175b008629105f
Gonzalo J. Paz Delgado <gonzalopd96@uma.es>
2022-12-21 11:13:35 +0100

    Dont smooth profile in DECOUPLED solution

--- src/MotionPlan.cpp
+++ src/MotionPlan.cpp
@@ -390,7 +390,7 @@ unsigned int MotionPlan::computeArmProfilePlanning()
                     << "[MM] \033[35m[----------] [MotionPlan::computeArmProfilePlanning()]\033[0m "
                        "Raw Profile is safe, with "
                     << this->vvd_arm_motion_profile.size() << " samples" << std::endl;
-                if(this->vvd_arm_motion_profile.size() > this->ui_gauss_numsamples * 2)
+                if(this->vvd_arm_motion_profile.size() > this->ui_gauss_numsamples * 2 && ui_deployment != 3)
                 {
                     this->p_arm_planner->computeArmProfileGaussSmoothening(
                         &(this->vvd_arm_motion_profile),
